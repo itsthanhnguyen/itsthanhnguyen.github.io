bbded245ed05edc9bc93db5d99b1f114089cd62f
Thanh Nguyen <chithanh1811@gmail.com>
2020-08-11 12:48:03 +0200

    Bottom toolbar Cassi

--- js/script.js
+++ js/script.js
@@ -1,44 +1,60 @@
 function expandNav() {
-    $(".navigation-flyout > ul").css({
-        "visibility": "visible"
-    });
-    $(".navigation-flyout").css({
-        "opacity": "1",
-        "z-index" : "9"
-    });
-    $('body').css({
-        "overflow": "hidden"
-    });
-    $("nav").css({
-        "background-color": "transparent",
-        "backdrop-filter": " ",
-        "-webkit-backdrop-filter": " "
-    })
-   
+    if ($(window).width() < 768) {
+        $(".navigation-flyout > ul").css({
+            "visibility": "visible"
+        });
+        $(".navigation-flyout").css({
+            "opacity": "1",
+            "z-index" : "9"
+        });
+        $('body').css({
+            "overflow": "hidden"
+        });
+        $("nav").css({
+            "background-color": "transparent",
+            "backdrop-filter": " ",
+            "-webkit-backdrop-filter": " "
+        })
+    } else {
+        $(".content-table").css({
+            "transform": "translateX(0rem)",
+            "margin-left": "0rem"
+        });
+        
+    }
 }
 
 function collapseNav() {
-    $(".navigation-flyout > ul").css({
-        "visibility": "hidden"
-    });
-    $(".navigation-flyout").css({
-        "opacity": "0",
-        "z-index" : "-10"
-    });
-    $('body').css({
-        "overflow": "auto"
-    });
-    $("nav").css({
-        "background-color": "var(--nav-bg-color)",
-        "backdrop-filter": "blur(30px) saturate(125%)",
-        "-webkit-backdrop-filter": "blur(30px) saturate(125%)"
-    })
+    if ($(window).width() < 768) {
+        $(".navigation-flyout > ul").css({
+            "visibility": "hidden"
+        });
+        $(".navigation-flyout").css({
+            "opacity": "0",
+            "z-index" : "-10"
+        });
+        $('body').css({
+            "overflow": "auto"
+        });
+        $("nav").css({
+            "background-color": "var(--nav-bg-color)",
+            "backdrop-filter": "blur(30px) saturate(125%)",
+            "-webkit-backdrop-filter": "blur(30px) saturate(125%)"
+        })
+    } else {
+        $(".content-table").css({
+            "transform": "translateX(-20rem)",
+            "margin-left": "-20rem"
+        });
+    }
 }
 
 $(document).ready(function() {
-  
-
-    var navCollapsed = true;
+    if ($(window).width() < 768) {
+        var navCollapsed = true;
+    } else {
+        var navCollapsed = false;
+    }
     $(".icon-menu").click(function(e) {
         if (navCollapsed){
             expandNav()
