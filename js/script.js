04373e06b2232213c99faacfbf5845587b1c8b72
Thanh Nguyen <chithanh1811@gmail.com>
2020-08-14 01:31:05 +0200

    WebP

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function expandNav() {
         });
         $(".navigation-flyout").css({
             "opacity": "1",
-            "z-index" : "9"
+            "z-index": "9"
         });
         $('body').css({
             "overflow": "hidden"
@@ -20,7 +20,7 @@ function expandNav() {
             "transform": "translateX(0rem)",
             "margin-right": "0rem"
         });
-        
+
     }
 }
 
@@ -31,7 +31,7 @@ function collapseNav() {
         });
         $(".navigation-flyout").css({
             "opacity": "0",
-            "z-index" : "-10"
+            "z-index": "-10"
         });
         $('body').css({
             "overflow": "auto"
@@ -49,14 +49,14 @@ function collapseNav() {
     }
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     if ($(window).width() <= 768) {
         var navCollapsed = true;
     } else {
         var navCollapsed = false;
     }
-    $(".icon-format_list_numbered").click(function(e) {
-        if (navCollapsed){
+    $(".icon-format_list_numbered").click(function (e) {
+        if (navCollapsed) {
             expandNav()
             navCollapsed = false;
         } else {
@@ -64,13 +64,13 @@ $(document).ready(function() {
             navCollapsed = true;
         }
     });
-    $(".navigation-flyout > ol > li").click(function(e) {
-        if (!navCollapsed){
+    $(".navigation-flyout > ol > li").click(function (e) {
+        if (!navCollapsed) {
             collapseNav()
             navCollapsed = true;
         }
     });
-    $(".icon-font-size-1").click(function(e) {
+    $(".icon-font-size-1").click(function (e) {
         $(":root").css({
             "font-size": "18px"
         });
@@ -84,7 +84,7 @@ $(document).ready(function() {
             "color": "var(--grey-1)"
         });
     });
-    $(".icon-font-size-2").click(function(e) {
+    $(".icon-font-size-2").click(function (e) {
         $(":root").css({
             "font-size": "20px"
         });
@@ -98,7 +98,7 @@ $(document).ready(function() {
             "color": "var(--grey-1)"
         });
     });
-    $(".icon-font-size-3").click(function(e) {
+    $(".icon-font-size-3").click(function (e) {
         $(":root").css({
             "font-size": "24px"
         });
@@ -143,98 +143,114 @@ function switchTheme() {
         colorScheme = "dark";
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
-        $(".switch").removeClass("icon-moon-fill");
-        $(".switch").addClass("icon-sun-fill");
-        $(".dark-img").css({
-            "display": "block"
-        });
-        $(".light-img").css({
-            "display": "none"
-        });
+        darkMode();
     } else {
         colorScheme = "light";
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
-        $(".switch").addClass("icon-moon-fill");
-        $(".switch").removeClass("icon-sun-fill");
-        $(".light-img").css({
-            "display": "block"
-        });
-        $(".dark-img").css({
-            "display": "none"
-        });
+        lightMode();
     }
 }
 
-$(document).ready(function() {
-    if (colorScheme == "dark") {
-        $(".switch").removeClass("icon-moon-fill");
-        $(".switch").addClass("icon-sun-fill");
-        $(".dark-img").css({
-            "display": "block"
-        });
-        $(".light-img").css({
-            "display": "none"
-        });
+function darkMode (){
+    $(".switch").removeClass("icon-moon-fill");
+    $(".switch").addClass("icon-sun-fill");
+    $(".dark-img").css({
+        "display": "block"
+    });
+    $(".light-img").css({
+        "display": "none"
+    });
+    $("#hashtag_design-process").html("<source srcset='images/hashtag_design-process-dark.webp' type='image/webp'> <source srcset='images/hashtag_design-process-dark.png' type='image/png'> <img src='images/hashtag_design-process-dark.png' alt='Development Process'></img>");
+    $("#hashtag_algorithm").html("<source srcset='images/hashtag_algorithm-dark.webp' type='image/webp'> <source srcset='images/hashtag_algorithm-dark.png' type='image/png'> <img src='images/hashtag_algorithm-dark.png' alt='Algorithm'>");  
+    $("#hashtag_system-architecture").html("<source srcset='images/hashtag_system-architecture-dark.webp' type='image/webp'> <source srcset='images/hashtag_system-architecture-dark.png' type='image/png'> <img src='images/hashtag_system-architecture-dark.png' alt='System Architecture'>");
+    
+    $("#cassiopeia_constraints_small").html("<source srcset='images/cassiopeia_constraints_small-dark.webp' type='image/webp'> <source srcset='images/cassiopeia_constraints_small-dark.png' type='image/png'> <img src='images/cassiopeia_constraints_small-dark.png' alt='Small-Sized Habitat'></img>");
+    $("#cassiopeia_constraints_slow").html("<source srcset='images/cassiopeia_constraints_slow-dark.webp' type='image/webp'> <source srcset='images/cassiopeia_constraints_slow-dark.png' type='image/png'> <img src='images/cassiopeia_constraints_slow-dark.png' alt='Slow Internet Connection'>");  
+    $("#cassiopeia_constraints_sad").html("<source srcset='images/cassiopeia_constraints_sad-dark.webp' type='image/webp'> <source srcset='images/cassiopeia_constraints_sad-dark.png' type='image/png'> <img src='images/cassiopeia_constraints_sad-dark.png' alt='Pronity to Mental Distress'>");
+    $("#cassiopeia_constraints_lab").html("<source srcset='images/cassiopeia_constraints_lab-dark.webp' type='image/webp'> <source srcset='images/cassiopeia_constraints_lab-dark.png' type='image/png'> <img src='images/cassiopeia_constraints_lab-dark.png' alt='Complex Laboratory Tasks'></img>");
+    $("#cassiopeia_system-architecture").html("<source srcset='images/cassiopeia_system-architecture-dark.webp' type='image/webp'> <source srcset='images/cassiopeia_system-architecture-dark.png' type='image/png'> <img src='images/cassiopeia_system-architecture-dark.png' alt='System Architecture'>");  
+
+}
+
+function lightMode (){
+    $(".switch").addClass("icon-moon-fill");
+    $(".switch").removeClass("icon-sun-fill");
+    $(".light-img").css({
+        "display": "block"
+    });
+    $(".dark-img").css({
+        "display": "none"
+    });
+    $("#hashtag_design-process").html("<source srcset='images/hashtag_design-process.webp' type='image/webp'> <source srcset='images/hashtag_design-process.png' type='image/png'> <img src='images/hashtag_design-process.png' alt='Development Process'></img>");
+    $("#hashtag_algorithm").html("<source srcset='images/hashtag_algorithm.webp' type='image/webp'> <source srcset='images/hashtag_algorithm.png' type='image/png'> <img src='images/hashtag_algorithm.png' alt='Algorithm'>");
+    $("#hashtag_system-architecture").html("<source srcset='images/hashtag_system-architecture.webp' type='image/webp'> <source srcset='images/hashtag_system-architecture.png' type='image/png'> <img src='images/hashtag_system-architecture.png' alt='System Architecture'>");
+
+    $("#cassiopeia_constraints_small").html("<source srcset='images/cassiopeia_constraints_small.webp' type='image/webp'> <source srcset='images/cassiopeia_constraints_small.png' type='image/png'> <img src='images/cassiopeia_constraints_small.png' alt='Small-Sized Habitat'></img>");
+    $("#cassiopeia_constraints_slow").html("<source srcset='images/cassiopeia_constraints_slow.webp' type='image/webp'> <source srcset='images/cassiopeia_constraints_slow.png' type='image/png'> <img src='images/cassiopeia_constraints_slow.png' alt='Slow Internet Connection'>");  
+    $("#cassiopeia_constraints_sad").html("<source srcset='images/cassiopeia_constraints_sad.webp' type='image/webp'> <source srcset='images/cassiopeia_constraints_sad.png' type='image/png'> <img src='images/cassiopeia_constraints_sad.png' alt='Pronity to Mental Distress'>");
+    $("#cassiopeia_constraints_lab").html("<source srcset='images/cassiopeia_constraints_lab.webp' type='image/webp'> <source srcset='images/cassiopeia_constraints_lab.png' type='image/png'> <img src='images/cassiopeia_constraints_lab.png' alt='Complex Laboratory Tasks'></img>");
+    $("#cassiopeia_system-architecture").html("<source srcset='images/cassiopeia_system-architecture.webp' type='image/webp'> <source srcset='images/cassiopeia_system-architecture.png' type='image/png'> <img src='images/cassiopeia_system-architecture.png' alt='System Architecture'>");  
+
+}
+
+$(document).ready(function () {
+    if (colorScheme == 'light') {
+        lightMode();
     } else {
-        $(".switch").addClass("icon-moon-fill");
-        $(".switch").removeClass("icon-sun-fill");
-        $(".light-img").css({
-            "display": "block"
-        });
-        $(".dark-img").css({
-            "display": "none"
-        });
+        darkMode();
     }
-    $(".switch").click(function(e) {
+    $(".switch").click(function (e) {
         switchTheme();
     });
 });
+
+// Scroll
 
 var prevScrollpos = window.pageYOffset;
 
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
     if (currentScrollPos < 500) {
-        $(".scroll-to-top").css({
-            "transform": "scale(0)",
-            "opacity": "0"
-        });
+                    $(".scroll-to-top").css({
+                        "transform": "scale(0)",
+                        "opacity": "0"
+                    });
     } else {
-        $(".scroll-to-top").css({
-            "transform": "scale(1)",
-            "opacity": "0.5"
-        });
+                    $(".scroll-to-top").css({
+                        "transform": "scale(1)",
+                        "opacity": "0.5"
+                    });
     }
     prevScrollpos = currentScrollPos;
 }
 
 $(document).ready(function() {
-    $(".scroll-to-top").click(function(e) {
-        $('html,body').animate({scrollTop:0},0);
-    });
+                    $(".scroll-to-top").click(function (e) {
+                        $('html,body').animate({ scrollTop: 0 }, 0);
+                    });
 });
 
 // Transition
 $(document).ready(function() {
-    console.log(window.location.href);
+                    console.log(window.location.href);
     if ($(window).width() <= 768 && window.location.href == "https://itsthanhnguyen.github.io/"){
-        $(".card").click(function(e) {
-            var navigateTo = "https://itsthanhnguyen.github.io/" + $(this)[0].id;
-            $('html, body').animate({
-                scrollTop: $("#"+$(this)[0].id).offset().top
-            }, 0, function() {
-                window.location.assign(navigateTo);
-            })
-        });
+                    $(".card").click(function (e) {
+                        var navigateTo = "https://itsthanhnguyen.github.io/" + $(this)[0].id;
+                        $('html, body').animate({
+                            scrollTop: $("#" + $(this)[0].id).offset().top
+                        }, 0, function () {
+                            window.location.assign(navigateTo);
+                        })
+                    });
     } else {
-        $(".card > div > button").click(function(e) {
-            var navigateTo = "https://itsthanhnguyen.github.io/" + $(this).parents().parents()[0].id;
-            $('html, body').animate({
-                scrollTop: $("#"+$(this).parents().parents()[0].id).offset().top
-            }, 0, function() {
-                window.location.assign(navigateTo);
-            })
-        });
+                    $(".card > div > button").click(function (e) {
+                        var navigateTo = "https://itsthanhnguyen.github.io/" + $(this).parents().parents()[0].id;
+                        $('html, body').animate({
+                            scrollTop: $("#" + $(this).parents().parents()[0].id).offset().top
+                        }, 0, function () {
+                            window.location.assign(navigateTo);
+                        })
+                    });
     }
 });
